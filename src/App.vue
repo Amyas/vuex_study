9fc9e02a6144decf8c79961186b933d0a2240779
王建鹏 <amyas.wang@qingclass.com>
2019-04-12 17:58:00 +0800

    子模块修改state UI

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h3>rootModule state count:{{$store.state.count}}</h3>
     <button @click="$store.commit('increment')">rootModule state count increment</button>
+    <h3>aModule state count:{{$store.state.a.count}}</h3>
+    <button @click="$store.commit('a/increment')">aModule state count increment</button>
   </div>
 </template>
 
